180e390a5e4df6c302a7c09c4ac7d6aaf8127d20
am-mahmud <am.mahmudasif@gmail.com>
2025-12-06 15:01:57 -0500

    Logo created

---
+++ src/components/shared/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <h1 className='font-bold text-sm sm:text-base md:text-lg lg:text-2xl text-gray-900 dark:text-white tracking-tight'>
+            contest sphere
+        </h1>
+    );
+};
+
+export default Logo;
