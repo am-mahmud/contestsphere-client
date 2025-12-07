3e956b216ecebb24ff6bf6d4ecb55ac9bbf0c7f9
am-mahmud <am.mahmudasif@gmail.com>
2025-12-07 09:22:10 -0500

    single contest

--- src/api/contest.js
+++ src/api/contest.js
@@ -5,8 +5,13 @@ export const contestAPI = {
     getAllContests: async (params = {}) => {
     const { data } = await api.get('/api/contests', { params });
     return data;
-    }
-    
+    },
+
+    // Get single contest
+  getContest: async (id) => {
+    const { data } = await api.get(`/api/contests/${id}`);
+    return data;
+  },
     
 
 }
