604ac1d62c29209143f015f0579520b02a72f139
am-mahmud <am.mahmudasif@gmail.com>
2025-12-07 09:21:10 -0500

    get all contest

--- src/api/contest.js
+++ src/api/contest.js
@@ -1,7 +1,12 @@
 import api from '../utils/api';
 
 export const contestAPI = {
- 
-
+    
+    getAllContests: async (params = {}) => {
+    const { data } = await api.get('/api/contests', { params });
+    return data;
+    }
+    
+    
 
 }
