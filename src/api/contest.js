c7de5cecefed62aa150c15d16ffbf0bc78aa6cf2
am-mahmud <am.mahmudasif@gmail.com>
2025-12-07 09:23:05 -0500

    create contest

--- src/api/contest.js
+++ src/api/contest.js
@@ -1,17 +1,22 @@
 import api from '../utils/api';
 
 export const contestAPI = {
-    
+
     getAllContests: async (params = {}) => {
-    const { data } = await api.get('/api/contests', { params });
-    return data;
+        const { data } = await api.get('/api/contests', { params });
+        return data;
     },
 
     // Get single contest
-  getContest: async (id) => {
-    const { data } = await api.get(`/api/contests/${id}`);
-    return data;
-  },
-    
+    getContest: async (id) => {
+        const { data } = await api.get(`/api/contests/${id}`);
+        return data;
+    },
+
+    // Create contest
+    createContest: async (contestData) => {
+        const { data } = await api.post('/api/contests', contestData);
+        return data;
+    },
 
 }
