beb3e8ce0379660936c31fff38f824b1bd155cdd
am-mahmud <am.mahmudasif@gmail.com>
2025-12-10 17:16:04 -0500

    file updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,3 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import { RouterProvider } from "react-router/dom";
-// import './index.css'
-// import { router } from './routes/Routes';
-
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-    
-//      <RouterProvider router={router} />
-//   </StrictMode>,
-// )
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router';
