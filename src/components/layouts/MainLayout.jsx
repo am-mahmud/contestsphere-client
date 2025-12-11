0677696bbae16e8b409dec43671f779c7f4188bb
am-mahmud <am.mahmudasif@gmail.com>
2025-12-11 15:41:42 -0500

    layout updated

--- src/components/layouts/MainLayout.jsx
+++ src/components/layouts/MainLayout.jsx
@@ -6,9 +6,9 @@ import { Outlet } from 'react-router';
 
 const MainLayout = () => {
   return (
-    <div className="flex flex-col min-h-screen max-w-7xl mx-auto">
+    <div className="flex flex-col min-h-screen">
       <Navbar />
-      <main className="grow">
+      <main className="grow max-w-7xl mx-auto">
         <Outlet />
       </main>
       <Footer />
